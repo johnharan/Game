21224db8f720ca8bc6ce684f98dcaeadb9e8505a
johnharan <j.haran44@gmail.com>
2015-09-18 22:45:37 +0100

    test commit from eclipse

--- TestEclipseGit/src/Test.java
+++ TestEclipseGit/src/Test.java
@@ -1,6 +1,7 @@
 
 public class Test {
 	/*
+	 * update 22:45 from eclipse
 	exercise from codingbat.com
  	Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, 
  	or return 0 if neither is in that range.
